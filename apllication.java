b592465d28e5bc7118c8061680ae4686bb79e0c8
bhosaletanvi <bhosaletanvi59@gmail.com>
2026-02-13 20:12:15 +0530

    Create apllication.java

---
+++ apllication.java
@@ -0,0 +1,12 @@
+package com.smma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmmaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmmaApplication.class, args);
+    }
+}
